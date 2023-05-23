8c64df0c8ca657646781fdf278b184c809b42177
roblesdotdev <httpsnip@gmail.com>
2023-05-23 19:37:41 -0300

    format with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="flex flex-col gap-4">
-        <button className="bg-transparent border-2 border-[#8556bc] px-6 py-3 rounded-lg text-white font-medium hover:bg-[#8556bc] outline-none focus:ring-2 ring-offset-4 focus:bg-[#8556bc] ring-offset-[#0e0c16] ring-[#8556bc] transition">
+        <button className="rounded-lg border-2 border-[#8556bc] bg-transparent px-6 py-3 font-medium text-white outline-none ring-[#8556bc] ring-offset-4 ring-offset-[#0e0c16] transition hover:bg-[#8556bc] focus:bg-[#8556bc] focus:ring-2">
           Get Started
         </button>
       </div>
     </main>
-  );
+  )
 }
